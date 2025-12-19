c51a903162575476156bd686852a4a05bf93e511
Erwin K. OKE <okekomerwin@gmail.com>
2025-12-19 12:06:45 +0000

    feat(seed db)

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { mockProducts } from "@/src/lib/mock-data";
+import 'dotenv/config'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function main() {
+  for (const u of mockProducts) {
+    await prisma.product.create({ data: u });
+  }
+}
+
+main();
